e7ec6652dba006641b7fc99440213267b65d4718
kaixwayvan <xanwayvan@gmail.com>
2025-01-16 19:19:06 +0800

    All features implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
 public class Main {
@@ -7,11 +5,9 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("");
-
         int bal = 100000, depo = 0, withdraw = 0;
 
-        System.out.println("Welcome to ABC BANK\n");
+        System.out.println("\nWelcome to ABC BANK\n");
         System.out.println("B - Check for balance");
         System.out.println("D - Make deposit");
         System.out.println("W - Make withdrawal");
@@ -26,13 +22,21 @@ public class Main {
                     System.out.println("\nYour current balance is " + bal + "\n");
 
                     System.out.print("Want to Transact Another (Y/N)? ");
-                    char transactOpt = Character.toUpperCase(scanner.nextLine().charAt(0));
+                    char transactOptB;
+                    while (true) {
+                        transactOptB = Character.toUpperCase(scanner.nextLine().charAt(0));
 
-                    if (transactOpt == 'Y') {
+                        if (!(transactOptB == 'Y' || transactOptB == 'N')) {
+                            System.out.print("\nInvalid entry, enter Y/N only: ");
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if (transactOptB == 'Y') {
                         System.out.println();
                         continue;
-                    } else if (transactOpt == 'N') {
-                        break;
                     }
 
                     break;
@@ -50,14 +54,22 @@ public class Main {
                         System.out.println("\nDeposit Amount must not be greater than 500,000");
                     }
 
-                    System.out.print("\nWant to Transact Another (Y/N)? ");
-                    char transactOpt1 = Character.toUpperCase(scanner.nextLine().charAt(0));
+                    System.out.print("Want to Transact Another (Y/N)? ");
+                    char transactOptD;
+                    while (true) {
+                        transactOptD = Character.toUpperCase(scanner.nextLine().charAt(0));
 
-                    if (transactOpt1 == 'Y') {
+                        if (!(transactOptD == 'Y' || transactOptD == 'N')) {
+                            System.out.print("\nInvalid entry, enter Y/N only: ");
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if (transactOptD == 'Y') {
                         System.out.println();
                         continue;
-                    } else if (transactOpt1 == 'N') {
-                        break;
                     }
 
                     break;
@@ -74,38 +86,58 @@ public class Main {
                         System.out.println("\nWithdrawal Amount must be exactly divisible by 100");
                     } else {
                         bal -= withdraw;
+                        System.out.println("\nWithdrawal Transaction is successfully completed.");
                     }
 
-                    System.out.print("\nWant to Transact Another (Y/N)? ");
-                    char transactOpt2 = Character.toUpperCase(scanner.nextLine().charAt(0));
+                    System.out.print("Want to Transact Another (Y/N)? ");
+                    char transactOptW;
+                    while (true) {
+                        transactOptW = Character.toUpperCase(scanner.nextLine().charAt(0));
 
-                    if (transactOpt2 == 'Y') {
+                        if (!(transactOptW == 'Y' || transactOptW == 'N')) {
+                            System.out.print("\nInvalid entry, enter Y/N only: ");
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if (transactOptW == 'Y') {
                         System.out.println();
                         continue;
-                    } else if (transactOpt2 == 'N') {
-                        break;
                     }
 
                     break;
                 case 'Q':
                     System.out.print("\nDo you really want to terminate this program (Y/N)? ");
-                    char quitOpt = Character.toUpperCase(scanner.nextLine().charAt(0));
 
-                    if (quitOpt == 'Y') {
-                        break;
-                    } else if (quitOpt == 'N') {
+                    char quitOpt;
+                    while (true) {
+                        quitOpt = Character.toUpperCase(scanner.nextLine().charAt(0));
+
+                        if (!(quitOpt == 'Y' || quitOpt == 'N')) {
+                            System.out.print("\nInvalid entry, enter Y/N only: ");
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if (quitOpt == 'N') {
                         System.out.println();
                         continue;
                     }
 
                     break;
+
                 default:
                     System.out.println("\nInvalid entry, enter any valid option: B/D/W/Q\n");
                     continue;
             }
 
             System.out.println("\nThank you for using the program!");
-            break;
+            scanner.close();
+            return;
 
         }
     }
